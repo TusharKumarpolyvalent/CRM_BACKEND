071381f90b6d6540a313416fbb947459c913a9e1
TusharKumarpolyvalent <tusharkumar@polyvalent.co.in>
2025-11-13 16:00:30 +0530

    build: add .env.local, .env.dev for easy switch for dev&local  variables

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.status(200).json({
+    message: '✅ Base route reached — Server is up and running smoothly!',
+  });
+});
+
+module.exports = app;
