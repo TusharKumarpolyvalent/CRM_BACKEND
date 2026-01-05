1e346bbea99a64a272739d434ae107065a8dbc93
Anushka-95 <anushkasingh@polyvalent.co.in>
2026-01-06 01:29:34 +0530

    refactor: first letter caps

--- prisma/migrations/20260105050945_add_passed_to_client/migration.sql
+++ prisma/migrations/20260105050945_add_passed_to_client/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE `campaign` ALTER COLUMN `updated_at` DROP DEFAULT;
+ALTER TABLE `Campaign` ALTER COLUMN `updated_at` DROP DEFAULT;
 
 -- AlterTable
-ALTER TABLE `leads` ADD COLUMN `passed_to_client` BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE `Leads` ADD COLUMN `passed_to_client` BOOLEAN NOT NULL DEFAULT false;
