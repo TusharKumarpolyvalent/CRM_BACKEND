dc5542aee8c157af7b80c096f33ec1154f976dea
Anushka-95 <anushkapolyvalent@gmail.co.in>
2026-02-18 14:56:51 +0530

    refactor: againprismaschema

--- prisma/migrations/20260218092557_fix_calllog_properly/migration.sql
+++ prisma/migrations/20260218092557_fix_calllog_properly/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE `LeadrRecord` ALTER COLUMN `updated_at` DROP DEFAULT;
+ALTER TABLE `LeadRecord` ALTER COLUMN `updated_at` DROP DEFAULT;
 
 -- AlterTable
 ALTER TABLE `Leads` ALTER COLUMN `updated_at` DROP DEFAULT;
